3777382754644564250612d1ebcddde9b911c114
Kevin Figueroa <kevinozmin@gmail.com>
2022-09-07 14:03:17 -0600

    some changes!

--- docker/apache/laravel_entrypoint.sh
+++ docker/apache/laravel_entrypoint.sh
@@ -2,8 +2,9 @@
 composer install
 npm install
 npm run dev
-php artisan migrate --seed
+#php artisan migrate --seed
 chmod -R 777 storage bootstrap
 a2ensite laravel_vhost.conf
 apache2ctl configtest
+certbot --apache -m support@makeitsimple.io -d dev-makeitsimple.io -d terraform.dev-makeitsimple.io
 apachectl -D FOREGROUND
