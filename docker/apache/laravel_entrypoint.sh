24b25dddd4b8c0622d20a987c5e52323ca98112e
Kevin Figueroa <kevinozmin@gmail.com>
2022-09-08 10:32:04 -0600

    certbot

--- docker/apache/laravel_entrypoint.sh
+++ docker/apache/laravel_entrypoint.sh
@@ -6,5 +6,5 @@ npm run dev
 chmod -R 777 storage bootstrap
 a2ensite laravel_vhost.conf
 apache2ctl configtest
-certbot --apache -m support@makeitsimple.io -d dev-makeitsimple.io -d terraform.dev-makeitsimple.io
+certbot --apache --redirect -m support@makeitsimple.io -d dev-makeitsimple.io -d terraform.dev-makeitsimple.io
 apachectl -D FOREGROUND
